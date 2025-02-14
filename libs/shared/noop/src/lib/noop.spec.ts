fdb32337a98b3496d2350528dbdd9924e5ec0596
Amir Al-Omari <me@amir.best>
2025-02-14 20:26:16 +0100

    chore(noop): remove project

--- libs/shared/noop/src/lib/noop.spec.ts
+++
@@ -1,7 +0,0 @@
-import { noop } from './noop';
-
-describe('noop', () => {
-  it('should work', () => {
-    expect(noop()).toEqual('noop');
-  });
-});
--- libs/shared/noop/src/lib/noop.ts
+++
@@ -1,3 +0,0 @@
-export function noop(): string {
-  return 'noop';
-}
